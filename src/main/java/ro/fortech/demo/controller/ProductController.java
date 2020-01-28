7f1681adc9e90c10fdd6645c95ac0a64f74296c2
Steli <mircea.nitu@fortech.ro>
2020-01-28 13:25:53 +0200

    Post method

--- src/main/java/ro/fortech/demo/controller/ProductController.java
+++ src/main/java/ro/fortech/demo/controller/ProductController.java
@@ -1,5 +1,7 @@
 package ro.fortech.demo.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,5 +22,10 @@ public class ProductController {
     @GetMapping
     public List<Product> getProducts(){
         return productService.getProducts();
+    }
+
+    @PostMapping
+    public Product addProduct(@RequestBody Product product) {
+        return productService.addProduct(product);
     }
 }
